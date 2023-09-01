b7e7a60cff9b7a7cd5e6da199ee21f7acc29c83e
Ritesh_Lakhani <114873105+RiteshLakhani@users.noreply.github.com>
2023-09-01 22:58:49 +0530

    Create 13.Roman_to_Integer.java

---
+++ 13.Roman_to_Integer.java
@@ -0,0 +1,34 @@
+//I can be placed before V (5) and X (10) to make 4 and 9. 
+//X can be placed before L (50) and C (100) to make 40 and 90. 
+//C can be placed before D (500) and M (1000) to make 400 and 900.
+
+class Solution {
+    public int romanToInt(String s) 
+    {
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+
+        int sum = 0 ;
+
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        s = s.replace("IV","IIII");
+        s = s.replace("IX","VIIII");
+        s = s.replace("XL","XXXX");
+        s = s.replace("XC","LXXXX");
+        s = s.replace("CD","CCCC");
+        s = s.replace("CM","DCCCC");
+
+        for(int i=0;i<s.length();i++)
+        {
+            sum = sum + (map.get(s.charAt(i)));
+        }    
+
+        return sum;
+    }
+}
